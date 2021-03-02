b249ef048bbd1869c8708759ec87bbf019a8b6a1
Ian Hong <ianqyhong@gmail.com>
2021-03-01 20:06:54 -0500

    moved components to src

---
+++ src/components/progbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const progbar = () => {
+    return (
+        <div>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%;"></div>
+            </div>
+        </div>
+    )
+}
+
+export default progbar
---
+++ src/components/prompt.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const prompt = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default prompt
