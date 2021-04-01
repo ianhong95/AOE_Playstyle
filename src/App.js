991bfe8a0c2946cce2ceabb58ca1523db71535a8
Ian Hong <ianqyhong@gmail.com>
2021-03-31 21:08:34 -0400

    States

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import MainContainer from './components/MainContainer'
 import Question1 from './components/Question1'
+import React, { useState } from 'react';
 
 // Find bootstrap template details on https://bootswatch.com/cyborg/
 
 function App() {
+
+  const [visible, setVisible] = useState(true);
+
   return (
     <div>
       {/* <MainContainer /> */}
-      <Question1 />
+      {{visible} ? <Question1 /> : null}
     </div>
   );
 }
--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
 const NextButton = ({ btnText }) => {
     return (
-        <button type="button" class="btn btn-outline-success btn-lg btn-block">{btnText}</button>
+        <button 
+            type="button" 
+            class="btn btn-outline-success btn-lg btn-block"
+            onClick={() => this.setVisible(false)}
+        >{btnText}</button>
     )
 }
 
